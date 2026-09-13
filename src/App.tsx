eb7adf3802766719209ac9e4857867a6d0c7c715
saharshg18 <goenkash18@gmail.com>
2026-09-14 01:18:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,25 @@ import { HomePage } from './pages/Home';
 import { ProductsPage } from './pages/Products';
 import { AboutPage } from './pages/About';
 
+export function AppContent() {
+  return (
+    <MotionConfig reducedMotion="user">
+      <SiteLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/about-us" element={<AboutPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </SiteLayout>
+    </MotionConfig>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <MotionConfig reducedMotion="user">
-        <SiteLayout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/about-us" element={<AboutPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </SiteLayout>
-      </MotionConfig>
+      <AppContent />
     </BrowserRouter>
   );
 }
